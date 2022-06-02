e4aa40bec7da108b76c11a08a0b96502bfcb8216
Khasbilegt.TS <khasbilegt.ts@gmail.com>
2022-06-02 12:49:45 +0800

    update noxfile

--- noxfile.py
+++ noxfile.py
@@ -13,13 +13,10 @@ def lint(session):
 @nox.session
 @nox.parametrize("django", ["3.2", "4.0", "main"])
 def test(session, django):
-    if django != "3.2" and not sys.version_info.minor >= 8:
-        session.skip()
+    if django == "main":
+        session.install("https://github.com/django/django/archive/main.tar.gz")  # noqa E501
     else:
-        if django == "main":
-            session.install("https://github.com/django/django/archive/main.tar.gz")  # noqa E501
-        else:
-            session.install(f"django=={django}")
-        session.run("coverage", "run", "-m", "pytest", "tests")
-        session.run("coverage", "report")
-        session.run("coverage", "xml")
+        session.install(f"django=={django}")
+    session.run("coverage", "run", "-m", "pytest", "tests")
+    session.run("coverage", "report")
+    session.run("coverage", "xml")
